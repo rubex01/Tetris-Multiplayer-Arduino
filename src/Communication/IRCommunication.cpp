d7493c3d63a19d5330624267467f11e2c9f98a1d
Ruben Pouwen <rbpouwen@gmail.com>
2021-12-01 12:23:05 +0100

    Works but only in one way

--- src/Communication/IRCommunication.cpp
+++ src/Communication/IRCommunication.cpp
@@ -1,60 +1,76 @@
-#ifndef __AVR_ATmega328P__
-#define __AVR_ATmega328P__
-#endif
-
 #include "Arduino.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include "HardwareSerial.h"
 #include "IRCommunication.h"
 
-ISR(INT0_vect)
+uint8_t deviderToGetRightHz;
+uint8_t OCRAValue;
+
+uint8_t data = 166;
+uint8_t dataLength = 8;
+uint8_t bitCount = 0;
+uint8_t sendCounter = 0;
+
+boolean currentlyReceiving = false;
+uint8_t numberOfCurrentReceivingBit = 0;
+uint8_t receiveCount = 0;
+boolean currentBitIsOne = true;
+uint8_t result = 0;
+
+void sendData()
 {
-    if (!IRCommunication::currentlyReceiving) {
-        IRCommunication::setCurrentlyReceiving(true); // Start receiving (start bit detected)
+    if (bitCount == 0) { // Start bit
+        TCCR0A |= (1<<COM0A0); // Enable compare interrupt
+        bitCount = 1; // Set bit to one as this is the first data bit that will be sent next cycle
         return;
     }
 
-    IRCommunication::toggleCurrentBitIsOne(); // Toggle on interrupt because there is a change in value
+    if (bitCount >= dataLength+1) { // End of Frame
+        TCCR0A &= ~(1<<COM0A0); // Set line to low for some time
+        bitCount++;
+        if (bitCount == dataLength+25) bitCount = 0; // Set bitcount to 0 for the next frame
+        return;
+    }
+
+    if ((data & (1<<(bitCount-1))) > 0) // Check if next data bit is 0 or 1
+        TCCR0A |= (1<<COM0A0); // Enable compare interrupt
+    else
+        TCCR0A &= ~(1<<COM0A0); // Disconnect
+
+    bitCount++; // Increase bit count for next data bit
+}
+
+ISR(INT0_vect)
+{
+    if (!currentlyReceiving) {
+        currentlyReceiving = true; // Start receiving (start bit detected)
+        return;
+    }
+
+    currentBitIsOne = !currentBitIsOne; // Toggle on interrupt because there is a change in value
+}
+
+void resetReceiver()
+{
+    numberOfCurrentReceivingBit = 0;
+    currentlyReceiving = false;
+    currentBitIsOne = true;
+    Serial.println(result);
+    result = 0;
 }
 
 ISR(TIMER0_COMPA_vect) {
-    IRCommunication::runSendForInterrupt();
-    IRCommunication::runReceiveForInterrupt();
-}
-
-int IRCommunication::kiloHertz = 38;
-boolean IRCommunication::currentlyReceiving = false;
-boolean IRCommunication::currentBitIsOne = true;
-int IRCommunication::dataLength = 8;
-int IRCommunication::numberOfCurrentReceivingBit = 0;
-uint8_t IRCommunication::result = 0;
-int IRCommunication::bitCount = 0;
-int IRCommunication::sendCounter = 0;
-int IRCommunication::receiveCount = 0;
-int IRCommunication::deviderToGetRightHz = 29; // 38 khz
-//int IRCommunication::deviderToGetRightHz = 42; // 56 khz
-uint8_t IRCommunication::data = 170;
-
-void IRCommunication::init()
-{
-    initPorts();
-    initTimer0();
-    initIrInterupt();
-}
-
-void IRCommunication::runSendForInterrupt()
-{
     if (sendCounter == deviderToGetRightHz) {
-        sendDataBit();
+        sendData();
         sendCounter = 0;
     }
     sendCounter++;
-}
 
-void IRCommunication::runReceiveForInterrupt()
-{
     if (currentlyReceiving) {
         if (
             (receiveCount == deviderToGetRightHz && numberOfCurrentReceivingBit != 0) ||
-            (numberOfCurrentReceivingBit == 0 && receiveCount == deviderToGetRightHz+(deviderToGetRightHz/2))
+            (numberOfCurrentReceivingBit == 0 && receiveCount == deviderToGetRightHz+(deviderToGetRightHz/2/2))
             ){ // Start sampling in middle of data bit
 
             if (currentBitIsOne && numberOfCurrentReceivingBit < dataLength)
@@ -70,68 +86,34 @@ void IRCommunication::runReceiveForInterrupt()
     }
 }
 
-void IRCommunication::toggleCurrentBitIsOne()
-{
-    currentBitIsOne = !currentBitIsOne;
-}
-
-void IRCommunication::resetReceiver()
-{
-    numberOfCurrentReceivingBit = 0;
-    currentlyReceiving = false;
-    currentBitIsOne = true;
-    Serial.println(result);
-    result = 0;
-}
-
-void IRCommunication::sendDataBit()
-{
-    if (bitCount == 0) { // Start bit
-        TCCR0A |= (1<<COM0A0); // Enable compare interrupt
-        bitCount = 1; // Set bit to one as this is the first data bit that will be sent next cycle
-        return;
-    }
-
-    if (bitCount >= dataLength+1) { // End of Frame
-        TCCR0A &= ~(1<<COM0A0); // Set line to low for some time
-        bitCount++;
-        if (bitCount == dataLength+20) bitCount = 0; // Set bitcount to 0 for the next frame
-        return;
-    }
-
-    if ((data & (1<<(bitCount-1))) > 0) // Check if next data bit is 0 or 1
-        TCCR0A |= (1<<COM0A0); // Enable compare interrupt
-    else
-        TCCR0A &= ~(1<<COM0A0); // Disconnect
-
-    bitCount++; // Increase bit count for next data bit
-}
-
-void IRCommunication::initPorts()
-{
-    DDRD |= (1<<DDD6);
-    PORTD |= (1<<PORTD2);
-}
-
-void IRCommunication::initTimer0()
+void initTimer0()
 {
     TCCR0A |= (1<<WGM00)|(1<<WGM01);
     TCCR0B |= (1<<WGM02);
     TCCR0A |= (1<<COM0A0);
     TCCR0B |= (1<<CS00);
-//    OCR0A = 141; // 56 khz
-    OCR0A = 206; // 38 khz
+    OCR0A = OCRAValue;
     TIMSK0 |= (1<<OCIE0A);
     TCNT0 = 0;
 }
 
-void IRCommunication::setCurrentlyReceiving(boolean value)
-{
-    currentlyReceiving = value;
-}
-
-void IRCommunication::initIrInterupt()
+void initIrInterupt()
 {
     EIMSK |= (1<<INT0);
     EICRA |= (1<<ISC00);
+}
+
+void initIRCommunication(int khz)
+{
+    deviderToGetRightHz = (khz == 56) ? 42 : 29;
+    OCRAValue = (khz == 56) ? 141 : 206;
+    DDRD |= (1<<DDD6);
+    PORTD |= (1<<PORTD2);
+
+    Serial.begin(9600);
+
+    sei();
+
+    initTimer0();
+    initIrInterupt();
 }
