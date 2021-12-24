fc5adf162ca80123b62d68fbc2fc042e97c37af4
Ruben Pouwen <rbpouwen@gmail.com>
2021-12-24 19:40:16 +0100

    Cleaned up Main

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,19 @@
 #define __AVR_ATmega328P__
 #endif
 
-#include "Arduino.h"
 #include <avr/interrupt.h>
 #include "Controller/Controller.h"
 #include "Display/Display.h"
 #include "Communication/IRCommunication.h"
 #include "Scenes/GameScene/GameScene.h"
-#include "Communication/ReceivedData.h"
 #include "Sound/NewTone.h"
-#include "Communication/Frame.h"
 #include "HighScore/HighScore.h"
-
-// #define ONESECOND 15  // Timer2 15 goeie, 1024 pre met ovf
-#define ONESECOND 20  // Timer2 15 goeie, 1024 pre met COMPA zonder pauze
 
 
 int main() {
-    NewTone::initTimer2();
     sei();
 
+    NewTone::initTimer2();
     Controller::init();
     Display::init();
     HighScore::init();
@@ -31,6 +25,7 @@ int main() {
     while (true) {
         Controller::update();
         Scene::draw();
+
         if (NewTone::startTone) {
             if (NewTone::toggleTone) {
                 NewTone::toggleTone = false;
@@ -38,5 +33,6 @@ int main() {
             }
         }
     }
+
     return(0);
 }
